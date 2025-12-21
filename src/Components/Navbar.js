020eb339bdb3ecf9466c5a8d1561bf3a8a884974
webxartist <webxartist@gmail.com>
2025-12-21 20:18:19 +0530

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="fixed  top-0 left-0 w-full bg-white shadow-md z-10">
+      <nav className="fixed  top-0 left-0 w-full bg-white shadow-md z-30">
         <div className="max-w-7xl mx-auto px-4">
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
@@ -97,7 +97,7 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       <div
-        className={`fixed top-16 left-0 w-full bg-white shadow-lg md:hidden transition-all duration-300 z-10 ${
+        className={`fixed top-16 left-0 w-full bg-white shadow-lg md:hidden transition-all duration-300 z-30 ${
           open
             ? "max-h-screen opacity-100"
             : "max-h-0 opacity-0 overflow-hidden"
