d6efca428ee93f0ce11f8674bef5696453023fd7
webxartist <webxartist@gmail.com>
2025-12-21 20:14:24 +0530

    z

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="fixed  top-0 left-0 w-full bg-white shadow-md z-[9999]">
+      <nav className="fixed  top-0 left-0 w-full bg-white shadow-md z-10">
         <div className="max-w-7xl mx-auto px-4">
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
